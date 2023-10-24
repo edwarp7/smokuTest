c4bfc133faca37cd63281f632b29aaae48b2fa08
changsam <edwarp7@naver.com>
2023-10-24 15:14:45 +0900

    add package-locok.json, package.json also ues app.get

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express'), http = require('http');
 // var cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+const { dirname } = require('path');
 var static = require('serve-static');
 var app = express();
 
@@ -20,6 +21,10 @@ const naverMapsProxy = createProxyMiddleware('/naver-maps-api', {
 
 // Use the proxy middleware
 app.use(naverMapsProxy);
+
+app.get('/', function(요청, 응답){
+  응답.sendFile(dirname + '/index.html');
+});
 
 http.createServer(app).listen(app.get('port'), app.get('host'), () => {
     console.log('Express server running at ' + app.get('port') + ' ' + app.get('host'));
