eb846ed2479f5b077411f7a9a799c726de69e636
changsam <edwarp7@naver.com>
2023-10-31 14:31:24 +0900

    change port num and delete host

--- app.js
+++ app.js
@@ -5,8 +5,8 @@ const { dirname } = require('path');
 var static = require('serve-static');
 var app = express();
 
-app.set('port', process.env.PORT || 8080);
-app.set('host', '127.0.0.1'); // 로컬 개발 환경에서는 host 설정을 주석 처리 또는 제거
+app.set('port', process.env.PORT || 80);
+//app.set('host', '127.0.0.1'); // 로컬 개발 환경에서는 host 설정을 주석 처리 또는 제거
 
 app.use(static(__dirname));
 
